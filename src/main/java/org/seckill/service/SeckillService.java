667387c83c373fc4e4ce33e0e6e5218607b6a1b6
Fzero1925 <fzero1925@gmail.com>
2017-01-13 23:29:12 +0800

    使用Spring MVC实现Restful接口完成

--- src/main/java/org/seckill/service/SeckillService.java
+++ src/main/java/org/seckill/service/SeckillService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
 
 /**
  * 业务接口：站在"使用者"的角度设计接口
@@ -41,7 +44,8 @@ public interface SeckillService {
 	 * @param md5
 	 * @return
 	 */
-	SeckillExecution executeSeckill(long seckillId, long userPhone, String md5);
+	SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
+		throws SeckillException,RepeatKillException,SeckillCloseException;
 
 }
 
